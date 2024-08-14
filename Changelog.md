bf9908017006436b1001a47ef3e2c338b9420e88
Roy Hagen <royahagen@gmail.com>
2024-08-14 03:26:29 +0200

    11.0

---
+++ Changelog.md
@@ -0,0 +1,25 @@
+## Changelog
+
+-- 2.0 --
+* Updated for WW Pre-Patch
+
+-- 1.3.0a --
+* interface version update
+
+-- 1.3.0 --
+* Updated for pre-patch 9.0.1
+
+-- 1.2.0 --
+* Despite not being on Mrrl, added info on Budding Deepcoral, as it's a permanent buff, not a one-off and many people do not know.
+
+-- 1.1.0 --
+* Mousing over a dungeon keystone threw an error "a bad argument #2 to 'strsplit' (string expected, got nil)" Fixed
+* Added missing item Ominous Looking Tome
+
+-- 1.0.5 -- 
+* Added some missed items
+* Fixed some spacing issues
+* Fixed spellingerrors
+
+-- 1.0.0 --
+* Initial creation
---
+++ mrrl.lua
@@ -0,0 +1,149 @@
+local MrrlFirstLogin = CreateFrame("Frame", "MrrlFirstLoginFrame")
+local inNazjatar = false
+
+local function GameTooltip_OnTooltipSetItem(tooltip)
+	if not inNazjatar then return end
+	local itemName, ItemLink = GameTooltip:GetItem();
+	-- print(itemName.." : "..ItemLink)
+	if not ItemLink then return end
+	local itemString = string.match(ItemLink, "item[%-?%d:]+")
+	-- print(itemString)
+	local _, itemId = strsplit(":", itemString)
+	-- print(itemId)
+
+	if itemId and itemId == "168094" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("1x Scrying Stone")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "168091" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("1x Barnacled Lockbox")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "168092" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("Mardivas reagent")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "168093" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("3x Prismatic Manapearls")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "168095" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("Azerite power")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "168096" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Contains:")
+		tooltip:AddLine("different Nazjatar items.")
+		tooltip:AddLine("Might not open if you already have the unique item in your bags.")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "168097" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("1x Benthic Token")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "170158" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("7x Prismatic Manapearl")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "170101" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("1x Benthic Token")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "170162" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Contains:")
+		tooltip:AddLine("different Nazjatar items.")
+		tooltip:AddLine("Might not open if you already have the unique item in your bags.")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "170157" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("Mardivas reagent")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "170152" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("Abyssal Conch")
+		tooltip:AddLine("Gives 150 rep with Nazjatar faction")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "170159" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Becomes:")
+		tooltip:AddLine("3x Prismatic Manapearl")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "170153" then
+		tooltip:AddLine(" ") --blank line
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Gives 150 experience to each bodyguard")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "169202" then
+		tooltip:AddLine(" ")
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("Attention!")
+		tooltip:AddLine("This is the mount")
+		tooltip:AddLine(" Buy if you can, it will go away for a while")
+		tooltip:AddLine(" ")
+	end
+	if itemId and itemId == "169885" then
+		tooltip:AddLine(" ")
+		tooltip:AddLine("-- Mrrls Item Information --")
+		tooltip:AddLine("This is not consume on use!")
+		tooltip:AddLine("The buff is permanent.")
+		tooltip:AddLine(" ")
+	end
+end
+
+-- GameTooltip:HookScript("OnTooltipSetItem", GameTooltip_OnTooltipSetItem)
+local function OnEvent(self, event, ...)
+	if event == "ZONE_CHANGED" or "PLAYER_LOGIN" then
+		if GetSubZoneText() == "Newhome" then
+			-- print("Yes we are in newhome, enable the addon")
+			inNazjatar = true
+		else
+			-- print("We are no longer in newhome, disable the addon")
+			inNazjatar = false
+		end
+	end
+	-- if event == "PLAYER_LOGIN" then
+	-- 	print(event, GetSubZoneText())
+	-- end
+end
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, GameTooltip_OnTooltipSetItem)
+MrrlFirstLogin:RegisterEvent("ZONE_CHANGED")
+MrrlFirstLogin:RegisterEvent("PLAYER_LOGIN")
+MrrlFirstLogin:SetScript("OnEvent", OnEvent)
